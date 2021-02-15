d3574662beb589f1e18d1ebfb823300e0c9f2d0a
Artur Laksberg <arturl@microsoft.com>
2021-02-15 15:18:30 -0800

    A sample demonstrating how to use batched DTMF input in an adaptive dialogs bot

---
+++ samples/csharp_dotnetcore/adaptive-dialog/01.number-prompt/Dialogs/BatchNumericInput.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveExpressions.Properties;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Dialogs.Choices;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Templates;
+using Microsoft.Bot.Schema;
+using Newtonsoft.Json;
+using Microsoft.Bot.Builder.Dialogs.Adaptive.Input;
+
+namespace MultiTurnPromptBot.Dialogs
+{
+    public class BatchNumericInput : Dialog
+    {
+        [JsonConstructor]
+        public BatchNumericInput([CallerFilePath] string sourceFilePath = "", [CallerLineNumber] int sourceLineNumber = 0)
+            : base()
+        {
+            // enable instances of this command as debug break point
+            this.RegisterSourceLocation(sourceFilePath, sourceLineNumber);
+        }
+        [JsonProperty("prompt")]
+        public String Prompt { get; set; }
+
+        [JsonProperty("property")]
+        public StringExpression Property { get; set; }
+
+        /// <summary>
+        /// Complete input collection when encountering this symbol
+        /// </summary>
+        [JsonProperty("condition")]
+        public String TerminatingSymbol { get; set; }
+
+        /// <summary>
+        /// Complete input collection when the input length reaches this number
+        /// </summary>
+        [JsonProperty("maxLen")]
+        public int MaxLen { get; set; }
+
+        public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object dialogOptions = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (!string.IsNullOrEmpty(Prompt))
+            {
+                var activity = MessageFactory.Text(Prompt);
+                await dc.Context.SendActivityAsync(activity);
+            }
+            return EndOfTurn;
+        }
+
+        public override async Task<DialogTurnResult> ContinueDialogAsync(DialogContext dc, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var current = dc.State.GetStringValue("dialog._current_dtmf", string.Empty);
+
+            if (dc.Context.Activity.Text == TerminatingSymbol || current.Length >= MaxLen)
+            {
+                string path = Property.GetValue(dc.State);
+                if (path == string.Empty)
+                {
+                    throw new InvalidOperationException("Unable to save the DTMF value, incorrect ResultProperty expression");
+                }
+                else
+                {
+                    dc.State.SetValue(path, current);
+                    return await dc.EndDialogAsync(result: current, cancellationToken: cancellationToken);
+                }
+            }
+
+            // TODO: translate spoken numbers into digits
+
+            if (Regex.IsMatch(dc.Context.Activity.Text, "^[0123456789]{1}$"))
+            {
+                dc.State.SetValue("dialog._current_dtmf", $"{current}{dc.Context.Activity.Text}");
+            }
+
+            return EndOfTurn;
+        }
+    }
+
+}
--- samples/csharp_dotnetcore/adaptive-dialog/01.number-prompt/Dialogs/RootDialog.cs
+++ samples/csharp_dotnetcore/adaptive-dialog/01.number-prompt/Dialogs/RootDialog.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.IO;
-using AdaptiveExpressions.Properties;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Builder.Dialogs.Adaptive;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Actions;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Conditions;
 using Microsoft.Bot.Builder.Dialogs.Adaptive.Generators;
-using Microsoft.Bot.Builder.Dialogs.Adaptive.Input;
-using Microsoft.Bot.Builder.Dialogs.Adaptive.Templates;
 using Microsoft.Bot.Builder.LanguageGeneration;
 
 namespace MultiTurnPromptBot.Dialogs
@@ -54,79 +51,36 @@ namespace MultiTurnPromptBot.Dialogs
                             Condition = "$foreach.value.name != turn.activity.recipient.name",
                             Actions = new List<Dialog>()
                             {
-                                new SendActivity("Hello, I'm the multi-turn prompt bot. Please send a message to get started!")
+                                new SendActivity("Hello, I'm a telephony test bot. Please send a message to get started!")
                             }
                         }
                     }
                 }
             };
-
         }
 
         private static List<Dialog> GatheUserInformation()
         {
             return new List<Dialog>()
             {
-                // Ask for user's age and set it in user.userProfile scope.
-                new TextInput()
-                {
-                    Prompt = new ActivityTemplate("${ModeOfTransportPrompt()}"),
-                    // Set the output of the text input to this property in memory.
-                    Property = "user.userProfile.Transport"
+                new BatchNumericInput() {
+                    Prompt = "What is your phone number? Please say or dial using the numerical keypad. Press # when finished",
+                    Property = "user.userProfile.Phone",
+                    TerminatingSymbol = "#",    // terminate after seeing '#'
+                    MaxLen = 10                 // ...or when the input length reaches 10
                 },
-                new TextInput()
-                {
-                    Prompt = new ActivityTemplate("${AskForName()}"),
-                    Property = "user.userProfile.Name"
-                },
-                // SendActivity supports full language generation resolution.
-                // See here to learn more about language generation
-                // https://aka.ms/language-generation
-                new SendActivity("${AckName()}"),
-                new ConfirmInput()
-                {
-                    Prompt = new ActivityTemplate("${AgeConfirmPrompt()}"),
-                    Property = "turn.ageConfirmation"
-                },
+                new SendActivity("Thanks, I got your phone as '${user.userProfile.Phone}'"),
                 new IfCondition()
                 {
-                    // All conditions are expressed using adaptive expressions.
-                    // See https://aka.ms/adaptive-expressions to learn more
-                    Condition = "turn.ageConfirmation == true",
+                    Condition = "startsWith(user.userProfile.Phone, '425')",
                     Actions = new List<Dialog>()
                     {
-                         new NumberInput()
-                         {
-                             Prompt = new ActivityTemplate("${AskForAge()}"),
-                             Property = "user.userProfile.Age",
-                             // Add validations
-                             Validations = new List<BoolExpression>()
-                             {
-                                 // Age must be greater than or equal 1
-                                 "int(this.value) >= 1",
-                                 // Age must be less than 150
-                                 "int(this.value) < 150"
-                             },
-                             InvalidPrompt = new ActivityTemplate("${AskForAge.invalid()}"),
-                             UnrecognizedPrompt = new ActivityTemplate("${AskForAge.unRecognized()}")
-                         },
-                         new SendActivity("${UserAgeReadBack()}")
+                        new SendActivity("I think you're in Redmond WA"),
                     },
                     ElseActions = new List<Dialog>()
                     {
-                        new SendActivity("${NoName()}")
+                        new SendActivity("I think you're form somewhere else"),
                     }
-                },
-                new ConfirmInput()
-                {
-                    Prompt = new ActivityTemplate("${ConfirmPrompt()}"),
-                    Property = "turn.finalConfirmation"
-                },
-                // Use LG template to come back with the final read out.
-                // This LG template is a great example of what logic can be wrapped up in LG sub-system.
-                new SendActivity("${FinalUserProfileReadOut()}"), // examines turn.finalConfirmation
-                new DeleteProperty() {
-                    Property = "user.userProfile"
                 },
                 new EndDialog(),
             };
