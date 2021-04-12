303a3335b66c6177feed6408de09ed722893b77e
Jordan Berger <jordanberger@microsoft.com>
2021-04-12 09:22:46 -0500

    Added example voice model

--- samples/javascript_nodejs/01.telephony-echo/src/bot.js
+++ samples/javascript_nodejs/01.telephony-echo/src/bot.js
@@ -6,10 +6,23 @@ const { ActivityHandler, MessageFactory } = require("botbuilder");
 class EchoBot extends ActivityHandler {
   constructor() {
     super();
+
+    const voiceModel = (text, voiceId, locale) => `
+      <speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" xml:lang="${locale}">
+        <voice name="${voiceId}">
+          ${text}
+        </voice>
+      </speak>`;
+
     // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
     this.onMessage(async (context, next) => {
       const replyText = `Echo: ${context.activity.text}`;
-      await context.sendActivity(MessageFactory.text(replyText, replyText));
+      await context.sendActivity(
+        MessageFactory.text(
+          replyText,
+          voiceModel(replyText, "en-US-AriaNeural", "en-US")
+        )
+      );
       // By calling next() you ensure that the next BotHandler is run.
       await next();
     });
@@ -20,7 +33,10 @@ class EchoBot extends ActivityHandler {
       for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
         if (membersAdded[cnt].id !== context.activity.recipient.id) {
           await context.sendActivity(
-            MessageFactory.text(welcomeText, welcomeText)
+            MessageFactory.text(
+              welcomeText,
+              voiceModel(welcomeText, "en-US-AriaNeural", "en-US")
+            )
           );
         }
       }
