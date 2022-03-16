c821ef6151ef61101743b7625acb35c7324edfb5
Joy Liu <ruicliu@microsoft.com>
2022-03-16 11:51:44 -0700

    Added logic for custom substitutions and unit tests to cover scenarios where substitutions will be performed

---
+++ experimental/SerialNumPostProcessingSkillSample/SpeechSerialNumber/Substitution.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using Newtonsoft.Json;
+
+namespace SpeechSerialNumber
+{
+    [JsonObject]
+    public class Substitution
+    {
+        [JsonProperty("substring")]
+        public string Substring { get; } = string.Empty;
+
+        [JsonProperty("replace_with")]
+        public string ReplaceWith { get; } = string.Empty;
+
+        public Substitution(string substring, string replaceWith)
+        {
+            this.Substring = substring;
+            this.ReplaceWith = replaceWith;
+        }
+    }
+}
--- experimental/SerialNumPostProcessingSkillSample/SpeechSerialNumberTests/SerialNumberPatternTests.cs
+++ experimental/SerialNumPostProcessingSkillSample/SpeechSerialNumberTests/SerialNumberPatternTests.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
 using SpeechSerialNumber;
 
 namespace SpeechSerialNumberTests
@@ -10,6 +12,28 @@ namespace SpeechSerialNumberTests
     [TestClass]
     public class SerialNumberPatternTests
     {
+        private static readonly string SubstitutionEnglishFilePath = Path.Combine(".", "substitution-en-us.json");
+
+        [TestInitialize]
+        public void CreateEnglishSubstitutionFile()
+        {
+            Substitution[] substitutions = { new Substitution("DENIED", "D9"), new Substitution("SEE", "C") };
+            Dictionary<string, Substitution[]> substitutionsJsonKVP = new Dictionary<string, Substitution[]>();
+            substitutionsJsonKVP.Add("substitutions", substitutions);
+
+            using (StreamWriter fs = File.CreateText(SubstitutionEnglishFilePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(fs, substitutionsJsonKVP);
+            }
+        }
+
+        [TestCleanup]
+        public void DeleteEnglishSubstitutionFile()
+        {
+            this.DeleteSubstitutionFileForEnglish();
+        }
+
         [TestMethod]
         public void MilitaryCodeTest()
         {
@@ -134,6 +158,116 @@ namespace SpeechSerialNumberTests
             result = pattern.Inference("A as in Apple ONE CR 00703 F");
             Assert.IsTrue(result.Length == 1);
             Assert.AreEqual(result[0], "A1CR00703F");
+        }
+
+        [TestMethod]
+        public void HPSerialNumberTestWithoutCustomSubstitutionFile()
+        {
+            this.DeleteSubstitutionFileForEnglish();
+
+            var groups = new List<SerialNumberTextGroup>();
+            var g1 = new SerialNumberTextGroup
+            {
+                AcceptsDigits = true,
+                AcceptsAlphabet = true,
+                LengthInChars = 10,
+            };
+            groups.Add(g1);
+
+            var pattern = new SerialNumberPattern(groups.AsReadOnly());
+            var result = pattern.Inference("A as in Apple 123456789");
+            Assert.IsTrue(result.Length == 2);
+            Assert.AreEqual(result[0], "A1234567A9");
+            Assert.AreEqual(result[1], "A123456789");
+
+            result = pattern.Inference("ONE CR 00703 F 3.");
+            Assert.IsTrue(result.Length == 1);
+            Assert.AreEqual(result[0], "1CR00703F3");
+        }
+
+        [TestMethod]
+        public void EnglishCustomSubstitutionWithDigitWordReplacement()
+        {
+            var groups = new List<SerialNumberTextGroup>();
+            var g1 = new SerialNumberTextGroup
+            {
+                AcceptsDigits = true,
+                AcceptsAlphabet = true,
+                LengthInChars = 10,
+            };
+            groups.Add(g1);
+
+            var pattern = new SerialNumberPattern(groups.AsReadOnly());
+
+            var result = pattern.Inference("ONE DENIED ONE 4 FIVE 6 SEE 1 SEE");
+            Assert.IsTrue(result.Length == 1);
+            Assert.AreEqual(result[0], "1D91456C1C");
+        }
+
+        [TestMethod]
+        public void EnglishCustomSubstitutionWithMilitaryCode()
+        {
+            var groups = new List<SerialNumberTextGroup>();
+            var g1 = new SerialNumberTextGroup
+            {
+                AcceptsDigits = true,
+                AcceptsAlphabet = true,
+                LengthInChars = 10,
+            };
+            groups.Add(g1);
+
+            var pattern = new SerialNumberPattern(groups.AsReadOnly());
+
+            var result = pattern.Inference("ONE DENIED A AS IN APPLE 4 FIVE 6 SEE 1 SEE");
+            Assert.IsTrue(result.Length == 1);
+            Assert.AreEqual(result[0], "1D9A456C1C");
+        }
+
+        [TestMethod]
+        public void EnglishCustomSubstitutionWitAmbiguousInput()
+        {
+            var groups = new List<SerialNumberTextGroup>();
+            var g1 = new SerialNumberTextGroup
+            {
+                AcceptsDigits = true,
+                AcceptsAlphabet = true,
+                LengthInChars = 10,
+            };
+            groups.Add(g1);
+
+            var pattern = new SerialNumberPattern(groups.AsReadOnly());
+
+            var result = pattern.Inference("ONE DENIED A 4 FIVE 8 SEE 1 SEE"); // 8 is ambiguous input
+            Assert.IsTrue(result.Length == 2);
+            Assert.AreEqual(result[0], "1D9A45AC1C");
+            Assert.AreEqual(result[1], "1D9A458C1C");
+        }
+
+        [TestMethod]
+        public void PostProcessedOutputShouldBeTruncatedToPatternLength()
+        {
+            var groups = new List<SerialNumberTextGroup>();
+            var g1 = new SerialNumberTextGroup
+            {
+                AcceptsDigits = true,
+                AcceptsAlphabet = true,
+                LengthInChars = 10,
+            };
+            groups.Add(g1);
+
+            var pattern = new SerialNumberPattern(groups.AsReadOnly());
+
+            var result = pattern.Inference("ONE DENIED A 4 FIVE 6 SEE 1 DENIED"); // 9 at the end should be truncated
+            Assert.IsTrue(result.Length == 1);
+            Assert.AreEqual(result[0], "1D9A456C1D");
+        }
+
+        private void DeleteSubstitutionFileForEnglish()
+        {
+            if (File.Exists(SubstitutionEnglishFilePath))
+            {
+                File.Delete(SubstitutionEnglishFilePath);
+            }
         }
     }
 }
